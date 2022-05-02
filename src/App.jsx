0279a85078ed5eea35bbe5cc72418c25790c63ab
Gabriel Neves <gabriel.neves@zrp.com.br>
2022-05-02 17:39:50 -0300

    ScrollComponent adicionado no app

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import { useEffect } from 'react'
 import './App.css'
 import { Header } from './components/Header'
 import { Home } from './pages/Home'
+import ScrollComponent from './components/InfiniteScrolling'
 
 const App = () => (
   <>
     <Header />
     <Home />
+    <ScrollComponent/>
   </>
 )
 
